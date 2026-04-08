e33b0ddbe1069b7a9f09f3fcaca1e657449e4f81
soroar42 <soroar42@gmail.com>
2026-04-08 12:48:36 +0600

    update library

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,6 @@
-checkout([
-    $class: 'GitSCM',
-    branches: [[name: '*/main']],
-    userRemoteConfigs: [[
-        url: 'https://github.com/your-username/your-shared-library.git'
-    ]]
-])
+def call(Map stageParams) {
+    checkout([
+        $class: 'GitSCM', 
+        branches: [[name: stageParams.branch]],
+        userRemoteConfigs: [[url: stageParams.url]]])
+}
