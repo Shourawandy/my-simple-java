1b12a15907b920aee9be3b313b812f6ee305cda4
soroar42 <soroar42@gmail.com>
2026-04-08 21:17:23 +0600

    new shared library file added for maven intregration

--- vars/staticCodeAnalaysis.groovy
+++ vars/staticCodeAnalaysis.groovy
@@ -1,10 +1,15 @@
 def call() {
-    echo "🚀 Starting SonarQube Static Code Analysis..."
+    echo " Starting SonarQube Static Code Analysis..."
 
-    // Change 'SonarQube' below to the exact name you configured in Jenkins
     withSonarQubeEnv('sonar-qube-api') {
-        sh 'mvn clean verify sonar:sonar'
+        withCredentials([string(credentialsId: 'sonar-qube-test', variable: 'SONAR_TOKEN')]) {
+            sh '''
+                mvn clean verify sonar:sonar \
+                    -Dsonar.projectKey=todo-web-app \
+                    -Dsonar.token=$SONAR_TOKEN
+            '''
+        }
     }
 
-    echo "✅ SonarQube analysis completed."
+    echo " SonarQube analysis completed."
 }
