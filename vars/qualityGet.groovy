745095fd36e82e88892af774cda5a2028dc59fe5
soroar42 <soroar42@gmail.com>
2026-04-09 08:42:48 +0600

    new shared library file added for maven intregration

--- vars/qualityGet.groovy
+++ vars/qualityGet.groovy
@@ -1,5 +1,12 @@
-def call() { 
-    echo "⏳ Waiting for SonarQube Quality Gate results..."
-    waitForQualityGate abortPipeline: false, credentialsId: 'sonar-qube-test'
-    echo "✅ Quality Gate check completed."}
-
+stage("Quality Gate: SonarQube") {
+    steps {
+        script {
+            // This step waits for the SonarQube analysis to be processed
+            // and returns the status (PASSED, WARN, or FAILED)
+            def qg = waitForQualityGate() 
+            if (qg.status != 'OK') {
+                error "Pipeline aborted due to quality gate failure: ${qg.status}"
+            }
+        }
+    }
+}
