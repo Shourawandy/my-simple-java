02e5bded24c34c1b1ef74d2abe1ee94c6c3e8c56
soroar42 <soroar42@gmail.com>
2026-04-09 17:41:10 +0600

    new shared library file added for maven intregration

---
+++ vars/dockerImageScan.groovy
@@ -0,0 +1,11 @@
+def call(String imageName, String imageTag, String hubUser) {
+
+    echo "Docker image scan is not implemented yet. This is a placeholder for future implementation."
+
+    sh """
+        traivy image  ${hubUser}/${imageName}:${imageTag} > scan-results.txt
+        cat scan-results.txt
+    """
+
+    echo "Docker image scan completed."
+}
